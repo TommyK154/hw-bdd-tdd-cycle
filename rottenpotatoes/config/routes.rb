f6ee61d994554b4a92cb87786856e2886c133297
tommyk154 <tkargul1@binghamton.edu>
2016-03-15 22:28:23 +0000

    part 1 done

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,9 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
+  #get 'movies/:id/find_by_director' => 'movies#find_by_director', 
+    #as => :find_by_director_movie
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
+  
+  get '/similar-movies/:id', to: 'movies#similar_movies', as: 'similar_movies'
 end
